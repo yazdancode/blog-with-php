de73c0e1b3023b3312c99b49eb334211ca5facaa
yazdancode <yshabanei@gmail.com>
2025-10-08 09:57:58 +0330

    .

--- admin-dashboard/Category.php
+++ admin-dashboard/Category.php
@@ -11,6 +11,7 @@ class Category extends Admin
     {
         $db = new Database();
         $category = $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
+        require_once realpath(__FILE__). "../template/admin/categories/index.php";
 
     }
 
@@ -18,12 +19,14 @@ class Category extends Admin
     {
         $db = new Database();
         $result = $db->select("SELECT * FROM `categories` WHERE `id` = ?", [$id]);
+        require_once realpath(__FILE__). "../template/admin/categories/show.php";
 
     }
 
 
     public function create():void
     {
+        require_once realpath(__FILE__). "../template/admin/categories/create.php";
 
     }
 
@@ -31,24 +34,28 @@ class Category extends Admin
     {
         $db = new Database();
         $db->insert('categories',array_keys($request), $request);
+        $this->redirect('category');
     }
 
     public function edit($id):void
     {
         $db = new Database();
         $result = $db->select("SELECT * FROM `categories` WHERE `id` = ?", [$id]);
+        require_once realpath(__FILE__). "../template/admin/categories/edit.php";
     }
 
     public function update($request, $id):void
     {
         $db = new Database();
         $db->update('categories', $id, array_keys($request), $request);
+        $this->redirect('category');
     }
 
     public function delete($id):void
     {
         $db = new Database();
         $db->delete('categories', $id);
+        $this->redirectBack();
     }
 
 }
