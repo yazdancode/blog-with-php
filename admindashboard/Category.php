266b78efc676995f8ee945f6a6d0a03a3077433c
yazdancode <yshabanei@gmail.com>
2025-10-08 16:32:57 +0330

    code true shod

--- admindashboard/Category.php
+++ admindashboard/Category.php
@@ -2,8 +2,12 @@
 
 namespace AdminDashboard;
 
+require_once __DIR__ . '/Admin.php';
+require_once __DIR__ . '/../Database/Database.php';
 
+use AdminDashboard\Admin;
 use Database\Database;
+
 
 class Category extends Admin
 {
@@ -11,7 +15,7 @@ class Category extends Admin
     {
         $db = new Database();
         $category = $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
-        require_once realpath(__FILE__). "../template/admin/categories/index.php";
+        require_once realpath(__FILE__). "/../template/admin/categories/index.php";
 
     }
 
@@ -19,43 +23,43 @@ class Category extends Admin
     {
         $db = new Database();
         $result = $db->select("SELECT * FROM `categories` WHERE `id` = ?", [$id]);
-        require_once realpath(__FILE__). "../template/admin/categories/show.php";
+        require_once realpath(__FILE__). "/../template/admin/categories/show.php";
 
     }
 
 
     public function create():void
     {
-        require_once realpath(__FILE__). "../template/admin/categories/create.php";
+        require_once realpath(__FILE__). "/../template/admin/categories/create.php";
 
     }
 
     public function store($request):void
     {
         $db = new Database();
-        $db->insert('categories',array_keys($request), $request);
-        $this->redirect('category');
+        $db->insert('categories', array_keys($request), $request);
+        // $this->redirect('category');
     }
 
     public function edit($id):void
     {
         $db = new Database();
         $result = $db->select("SELECT * FROM `categories` WHERE `id` = ?", [$id]);
-        require_once realpath(__FILE__). "../template/admin/categories/edit.php";
+        require_once realpath(__FILE__). "/../template/admin/categories/edit.php";
     }
 
     public function update($request, $id):void
     {
         $db = new Database();
         $db->update('categories', $id, array_keys($request), $request);
-        $this->redirect('category');
+        // $this->redirect('category');
     }
 
     public function delete($id):void
     {
         $db = new Database();
         $db->delete('categories', $id);
-        $this->redirectBack();
+        // $this->redirectBack();
     }
 
 }
