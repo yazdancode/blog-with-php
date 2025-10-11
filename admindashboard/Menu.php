65d9ce7add38854ff801249d59c5e9f88deed76d
yazdancode <yshabanei@gmail.com>
2025-10-11 20:57:22 +0330

    menu

---
+++ admindashboard/Menu.php
@@ -0,0 +1,43 @@
+<?php
+namespace Admindashboard;
+require_once __DIR__ . '/Admin.php';
+require_once __DIR__ . '/../Database/Database.php';
+use Database;
+
+class Menu extends Admin
+{
+    public function index()
+    {
+
+    }
+    public function show($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store($request): void
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($request, $id): void
+    {
+
+    }
+
+    public function delete($id): void
+    {
+
+    }
+
+}
