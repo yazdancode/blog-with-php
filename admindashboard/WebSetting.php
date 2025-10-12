20ec8341a61e8a3571f154b693bc2236a9738b90
yazdancode <yshabanei@gmail.com>
2025-10-12 15:17:05 +0330

    code changed

--- admindashboard/WebSetting.php
+++ admindashboard/WebSetting.php
@@ -27,37 +27,27 @@ class WebSetting extends Admin
         $setting = $db->select("SELECT * FROM websetting LIMIT 1");
         $settingExists = !empty($setting);
         $settingId = $settingExists ? $setting[0]['id'] : null;
-
-        // --- ذخیره‌ی لوگو ---
         if (isset($request['logo']) && !empty($request['logo']['tmp_name'])) {
             $request['logo'] = $this->saveImage($request['logo'], 'setting', 'logo');
         } else {
             unset($request['logo']);
         }
-
-        // --- ذخیره‌ی آیکون ---
         if (isset($request['icon']) && !empty($request['icon']['tmp_name'])) {
             $request['icon'] = $this->saveImage($request['icon'], 'setting', 'icon');
         } else {
             unset($request['icon']);
         }
-
-        // حذف مقادیر فایل اضافی برای جلوگیری از خطا
         if (isset($request['logo']['tmp_name'])) {
             unset($request['logo']['tmp_name']);
         }
         if (isset($request['icon']['tmp_name'])) {
             unset($request['icon']['tmp_name']);
         }
-
-        // --- درج یا به‌روزرسانی در دیتابیس ---
         if ($settingExists) {
             $db->update('websetting', $settingId, array_keys($request), $request);
         } else {
             $db->insert('websetting', array_keys($request), $request);
         }
-
-        // --- ریدایرکت به صفحه تنظیمات ---
         $this->redirect('web-setting');
     }
 }
